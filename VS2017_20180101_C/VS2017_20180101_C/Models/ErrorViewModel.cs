813bd522ac8bfd0eeb73e3d171488aaa2c600af9
Daniel Paduck <daniel.paduck@perkinswill.com>
2018-01-01 08:34:57 -0600

    Initial commit

---
+++ VS2017_20180101_C/VS2017_20180101_C/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VS2017_20180101_C.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
